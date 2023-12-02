ef48b950a5321727f1ee9ca41b9c1d0df620fea8
Noah <noahdevelopsthings@gmail.com>
2023-12-01 22:56:54 -0400

    Day 1

---
+++ src/day1.js
@@ -0,0 +1,95 @@
+const { CommandInteractionOptionResolver } = require('discord.js');
+const fs = require('fs');
+
+
+const input = fs.readFileSync('src/inputs/day1.txt',  {encoding: 'utf8', flag: 'r'});
+
+let p1 = 0;
+
+let lines = input.split('\n');
+
+for (let i = 0; i < lines.length; i++) {
+    let first, last
+    const line = lines[i]
+
+    for (let j = 0; j < line.length; j++) {
+        let digit = parseInt(line[j])
+        
+        if (isNaN(digit)) {
+            continue   
+        }
+     
+
+        if (first == undefined)  {
+            first = digit
+        }
+        
+        last = digit
+    }
+
+    p1 += parseInt(first.toString() + last.toString())
+}
+
+const lookingFor = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", 
+    1, 2, 3, 4, 5, 6, 7, 8, 9 
+]
+
+let p2 = 0
+
+const translations = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+]
+
+for (let i = 0; i < lines.length; i++) {
+
+    const line = lines[i]
+
+    let first, last
+    let firstIndex = Number.MAX_VALUE
+    let lastIndex = -1
+
+    lookingFor.forEach((value, key) => {
+        const low = line.indexOf(value)
+        const high = line.lastIndexOf(value)
+
+        let digit = key
+
+        if (!isNaN(parseInt(value))) {
+            digit = parseInt(value) - 1
+        }
+   
+        if (digit < 9) {
+            digit += 9
+        } 
+
+        let currentDigit = lookingFor[digit]
+    
+        if (low != -1 && firstIndex > low) {
+            first = currentDigit
+            firstIndex = low
+        }
+
+        if (high != -1 && lastIndex < high) {
+            last = currentDigit
+            lastIndex = high
+        }
+    })
+
+    p2 += parseInt(first.toString() + last.toString())
+ 
+}
+
+
+
+console.log("P1 Answer: " + p1)
+console.log("P2 Answer: " + p2)
+
